d83e06229bd4f2ac9ba58fec185661c2c18ec95f
monune <uppercase141@gmail.com>
2023-09-07 15:27:46 +0900

    puppteer 라이브러리 추가 및 테스트 파일 삭제

--- app/app.js
+++ app/app.js
@@ -14,8 +14,8 @@ const mysql = require('./database')();
 const connection = mysql.init();
 
 mysql.db_open(connection);
-
-const cheerio = require('cheerio');
+const puppeteer = require('puppeteer'); // 아아 나의 구원자
+const cheerio = require('cheerio');     // 
 const axios = require('axios');
 
 const getHTML = async (keyword) => {
@@ -28,18 +28,22 @@ const getHTML = async (keyword) => {
 
 // parsing 성공 
 const parsing = async (keyword) => {
+
   const html = await getHTML(keyword);
   const $ = cheerio.load(html.data);
-  const $weather = $(".status_wrap");
 
+  // Query 설정
+  const $weather = $(".status_wrap");
   const temperature = $weather.find("._today .temperature_text strong:eq(0)").text();
   const summaryList = $weather.find(".summary_list .sort").text();
   const weatherData = [];
 
+  // Class 계산
   const img = $weather.find("._today .weather_graphic .weather_main i").attr("class");
   const textArray = img.split(" "); // 공백 제거
   const imgClass = textArray[1].split("_"); // _ 제거
 
+  // 날씨 데이터 계산
   for (let i = 0; i < 4; i++) {
     const today = $weather.find(`li.item_today a:eq(${i})`).text().trim().split(" ");
     weatherData.push(today[1]);
@@ -60,52 +64,47 @@ const parsing = async (keyword) => {
     }
   }
 
-  // 습도 크롤링 / 나는 건들여서는 안되는걸 건들인게 아닐까? 이 문제가 나의 발목을 잡는다...
-  
-  const arr = [];
-  const $dataItems = $("div.open .content_area .inner .forecast_wrap .hourly_forecast .humidity_graph_box div.scroll_box._horizontal_scroll._hourly_humidity .climate_box div.graph_wrap ul");
-  for (let i = 0; i < 24; i++) {
-    const today = $dataItems.find(`li.data .data_inner span.base_bar:eq(${i})`).attr('style');
-    arr.push(today);
-    if (today >= 10) {
-    }
+  // 예상 습도 (시간) 가져오기
+  const today = new Date();
+  const timeArray = [];
+  const $dataItems = $(".humidity_graph_box .climate_box ");
+  for (let i = 0; i < 24 + (24 - today.getHours()); i++) {
+    const tarr = $dataItems.find(`.time_wrap .time span.text:eq(${i})`).text();
+    timeArray.push(tarr);
   }
-  console.log(arr);
-  
+  const Hpup_1 = await pup();
+  console.log("---------------------------------------")
+  console.log(timeArray);
+  console.log(Hpup_1);
 
-
-  // 날씨 데이터 갱신
-  const weatherColumns = [
-    'temperature', 'filling_temperature', 'meteoric_water', 'humidity',
-    'wind_direction', 'wind_speed', 'fine_dust', 'super_fine_dust',
-    'ultraviolet_rays', 'sunset', 'class'
-  ];
+  //
   const weatherValues = [tempReplace, ...Summary.slice(0, 5), ...weatherData.slice(0, 4), imgClass[1]];
-  updateDatabase('iemh_team1.weather', weatherColumns, weatherValues);
-  
-  // 습도 데이터 갱신
-  const humiColumns = [
-    'am12', 'am1', 'am2', 'am3', 'am4', 'am5', 'am6', 'am7', 'am8', 'am9', 'am10', 'am11',
-    'pm12', 'pm1', 'pm2', 'pm3', 'pm4', 'pm5', 'pm6', 'pm7', 'pm8', 'pm9', 'pm10', 'pm11'
+  const weatherColumns = [ 
+    'temperature', 'filling_temperature', 'meteoric_water', 'humidity',
+    'wind_direction', 'wind_speed', 'fine_dust', 'super_fine_dust', 
+    'ultraviolet_rays', 'sunset', 'class' 
   ];
-  // const humiValues = TimeArray.slice(0, 24);
-  // updateDatabase('iemh_team1.live_humidity', humiColumns, humiValues);
+  
+  //
+  const jsonData = JSON.stringify({ data: Hpup_1 });
+  const humidityColumns = ['docs'];
+  const humidityValues = [jsonData];
+  
+  //
+  const jsonData2 = JSON.stringify({ data: timeArray });
+  const timesColums = ['times'];
+  const timeValues = [jsonData2];
+  
+  // DB 갱신
+  updateDatabase('iemh_team1.weather', weatherColumns, weatherValues);
+  updateDatabase('iemh_team1.realtime_humidity', humidityColumns, humidityValues);
+  updateDatabase('iemh_team1.realtime_humidity', timesColums, timeValues);
+  
 };
 
-// 데이터 조회
-const retrieveData = () => {
-  return new Promise((resolve, reject) => {
-    let sql = `SELECT address FROM iemh_team1.weather`;
-    connection.query(sql, (error, results) => {
-      if (error) {
-        console.log(error);
-        reject(error);
-      } else {
-        resolve(results);
-      }
-    });
-  });
-};
+setInterval(() => {
+  parsing(`날씨`);
+}, 10000);
 
 /**
  * 데이터베이스 업데이트하는  함수
@@ -114,7 +113,7 @@ const retrieveData = () => {
  * @param {value} values 
  * @param {*} callback 
  */
-function updateDatabase(table, columns, values, callback) {
+ function updateDatabase(table, columns, values, callback) {
   const placeholders = columns.map(column => `${column} = ?`).join(', ');
   const sql = `UPDATE ${table} SET ${placeholders}`;
   connection.query(sql, values, (error, results) => {
@@ -129,8 +128,37 @@ function updateDatabase(table, columns, values, callback) {
   });
 }
 
-setInterval(async () => {
-  const data = await retrieveData();
-  const address = data[0].address;
-  parsing(`${address} 날씨`);
-}, 5000);
+/**
+ * puppetter 노드 라이브러리를 이용한 크롤링 함수
+ * @returns {array} 필터링된 배열 값
+ */
+const pup = async () => {  
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+  const url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EB%82%A0%EC%94%A8';
+  await page.goto(url);
+  
+  const PuppteerArray = page.evaluate(() => {
+    const numElements = Array.from(document.querySelectorAll('li.data .num'));
+    const numbers = numElements.map(element => parseInt(element.textContent, 10));
+    const filteredNumbers = numbers.filter(number => number >= 10);
+    const desiredLength = 24 + (24 - new Date().getHours());
+
+    // 10 미만의 숫자를 0으로 채웁니다.
+    while (filteredNumbers.length < desiredLength) filteredNumbers.push(0);
+    // 배열의 길이가 원하는 길이보다 더 길다면 잘라냅니다.
+    if (filteredNumbers.length > desiredLength) filteredNumbers.length = desiredLength;
+    return filteredNumbers;
+  });
+  return PuppteerArray;
+};
+
+/**
+ * 배열을 JSON 형식으로 변환하는 함수
+ * @param {array} arr 
+ */
+const changeJSON = (arr) => {
+  let data = JSON.stringify(arr); // '["Apple","Banana","Orange"]' // 배열 문자열
+  let data2 = JSON.parse(data);
+  return data2;
+}
