eff4019f6f7c7ad4b14c24e60d708ca9d956a72d
우현성 <123826926+monune@users.noreply.github.com>
2023-09-08 10:05:02 +0900

    ✅ 보안

---
+++ server/db_conn.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "servername";
+$username = "username";
+$password = "password";
+$dbname = "dbname";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
